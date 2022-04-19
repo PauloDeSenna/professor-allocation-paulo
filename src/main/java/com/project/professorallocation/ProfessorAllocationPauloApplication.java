f2490efd186a86bb06a61e83027f20c46780bb5a
Paulo de Senna <paulosennaneto@yahoo.com.br>
2022-04-18 22:16:29 -0300

    add initial spingboot web project files

---
+++ src/main/java/com/project/professorallocation/ProfessorAllocationPauloApplication.java
@@ -0,0 +1,13 @@
+package com.project.professorallocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationPauloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationPauloApplication.class, args);
+	}
+
+}
